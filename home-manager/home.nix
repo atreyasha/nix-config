9fec82382dbd78dfd1a6bf781b009c40856a7ffa
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-16 20:10:34 +0800

    Prettify home manager configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,62 +1,38 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{ inputs, lib, config, pkgs, user, ... }:
+
 {
-  inputs,
-  lib,
-  config,
-  pkgs,
-  user,
-  ...
-}: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
+  # modular imports
+  imports = [];
 
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
-
+  # configure nixpkgs as necessary
   nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
+      # we want unfree stuff
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+
+      # workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
     };
   };
 
-  # TODO: Add i3-cycle pip package
-  # TODO: Use autorandr from home manager
-  # TODO: Add appropriate home directories
-  # Set your username
+  # TODO: add i3-cycle pip package
+  # TODO: use autorandr from home manager
+  # TODO: add appropriate home directories or use XDG
+
+  # set your user's details
   home = {
     username = "${user}";
     homeDirectory = "/home/${user}";
+    packages = with pkgs; [ emacs ];
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  # enable home-manager and git
+  programs = {
+    home-manager.enable = true;
+    git.enable = true;
+  };
 
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
-
-  # Nicely reload system units when changing configs
+  # nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
