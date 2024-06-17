a3393d22bbb7cb35159ae2be78cb00ea8013cc35
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-17 11:32:19 +0800

    Update `git.nix`

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,5 +1,13 @@
 {
   programs.git = {
     enable = true;
+    userName = "Atreya Shankar";
+    userEmail = "35427332+atreyasha@users.noreply.github.com";
+    extraConfig = {
+      pull.rebase = true;
+      credential.helper = "cache";
+      commit.gpgsign = true;
+    };
+    lfs.enable = true;
   };
 }
