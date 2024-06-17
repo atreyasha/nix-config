9a6115e0db8c8eea8ddc348ea36efa12d79736e8
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-17 10:28:40 +0800

    Simplify `alacritty.nix`

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -1,65 +1,42 @@
+let
+  alacrittyFont = "BitstromWera Nerd Font Mono";
+in
 {
   programs.alacritty = {
     enable = true;
-    settings = ''
-      [colors.primary]
-      background = "#171717"
-      foreground = "#dcdccc"
+    settings = {
+      colors.primary = {
+        background = "#171717";
+        foreground = "#dcdccc";
+      };
 
-      [env]
-      WINIT_X11_SCALE_FACTOR = "1.0"
+      window = {
+        opacity = 0.97;
+      };
 
-      [font]
-      size = 12.0
+      env = {
+        WINIT_X11_SCALE_FACTOR = "1.0";
+      };
 
-      [font.bold]
-      family = "BitstromWera Nerd Font Mono"
-      style = "Bold"
-
-      [font.bold_italic]
-      family = "BitstromWera Nerd Font Mono"
-      style = "Bold Italic"
-
-      [font.italic]
-      family = "BitstromWera Nerd Font Mono"
-      style = "Italic"
-
-      [font.normal]
-      family = "BitstromWera Nerd Font Mono"
-      style = "Regular"
-
-      [[keyboard.bindings]]
-      action = "ScrollPageUp"
-      key = "PageUp"
-      mode = "~Alt"
-      mods = "Shift|Control"
-
-      [[keyboard.bindings]]
-      action = "ScrollPageDown"
-      key = "PageDown"
-      mode = "~Alt"
-      mods = "Shift|Control"
-
-      [[keyboard.bindings]]
-      action = "ScrollLineUp"
-      key = "Up"
-      mode = "~Alt"
-      mods = "Shift|Control"
-
-      [[keyboard.bindings]]
-      action = "ScrollLineDown"
-      key = "Down"
-      mode = "~Alt"
-      mods = "Shift|Control"
-
-      [[keyboard.bindings]]
-      action = "Last"
-      key = 5
-      mode = "Vi|~Search"
-      mods = "Shift"
-
-      [window]
-      opacity = 0.97
-      '';
+      font = {
+        size = 12.0;
+        bold = {
+          family = "${alacrittyFont}";
+          style = "Bold";
+        };
+        bold_italic = {
+          family = "${alacrittyFont}";
+          style = "Bold Italic";
+        };
+        italic = {
+          family = "${alacrittyFont}";
+          style = "Italic";
+        };
+        normal = {
+          family = "${alacrittyFont}";
+          style = "Regular";
+        };
+      };
+    };
   };
 }
