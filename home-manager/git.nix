9884ff86c7bea32f699cc14c4123348e5260f70e
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-17 15:09:29 +0800

    Minor bugfix faulty git config

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -6,7 +6,7 @@
     extraConfig = {
       pull.rebase = true;
       credential.helper = "cache";
-      signingkey = "0xFB88142808F7FAE8!";
+      user.signingkey = "0xFB88142808F7FAE8!";
       commit.gpgsign = true;
     };
     lfs.enable = true;
