296a647b1f23ac422668b9dda955d52666809cad
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-17 14:33:13 +0800

    Add signing key ID
    TODO: make this modular later

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -6,6 +6,7 @@
     extraConfig = {
       pull.rebase = true;
       credential.helper = "cache";
+      signingkey = "0xFB88142808F7FAE8!";
       commit.gpgsign = true;
     };
     lfs.enable = true;
