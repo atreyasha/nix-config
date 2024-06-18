f4fccb50b2976f10abc1f797f231fff223387a39
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-18 12:56:08 +0800

    Update `home-manager` name

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ranger-devicons = {
