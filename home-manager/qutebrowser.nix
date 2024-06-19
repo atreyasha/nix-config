65150e6cee6d20b8faf9d2e57344afed7c1e3867
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-19 13:57:30 +0800

    Remove problematic line from qutebrowser

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -1,11 +1,8 @@
-{ lib, ... }:
-
 {
   programs.qutebrowser = {
     enable = true;
     settings = {
       content.javascript.clipboard = "access-paste";
-      editor.command = lib.mkLiteral ''["i3-sensible-terminal", "-e", "vim", "{file}"]'';
     };
   };
 }
