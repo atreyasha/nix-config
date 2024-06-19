5fdc8e91b2efeccfe5d8a50ca6e65cbf8f1ea862
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-19 14:20:15 +0800

    We're good with qutebrowser

--- home-manager/qutebrowser.nix
+++ home-manager/qutebrowser.nix
@@ -16,17 +16,5 @@ in
     searchEngines = {
       DEFAULT = "https://www.google.com/search?q={}";
     };
-    # config.bind('K', 'tab-next')
-    # config.bind('J', 'tab-prev')
-    # config.bind('gK', 'tab-move +')
-    # config.bind('gJ', 'tab-move -')
-    # config.bind('<Ctrl-h>', 'scroll-page -0.2 0')
-    # config.bind('<Ctrl-j>', 'scroll-page 0 0.2')
-    # config.bind('<Ctrl-k>', 'scroll-page 0 -0.2')
-    # config.bind('<Ctrl-l>', 'scroll-page 0.2 0')
-    # config.bind('h', 'scroll-page -0.05 0')
-    # config.bind('j', 'scroll-page 0 0.05')
-    # config.bind('k', 'scroll-page 0 -0.05')
-    # config.bind('l', 'scroll-page 0.05 0')
   };
 }
