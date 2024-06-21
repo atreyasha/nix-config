61a074fd9721f869c8c6a29c50d80d051d51f64b
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2024-06-21 12:04:30 +0800

    Add support for `picom`

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,6 +8,7 @@
     ./git.nix
     ./gpg.nix
     ./htop.nix
+    ./picom.nix
     ./qutebrowser.nix
     ./ranger.nix
     ./readline.nix
---
+++ home-manager/picom.nix
@@ -0,0 +1,10 @@
+{
+  programs.picom = {
+    enable = true;
+    fade = true;
+    fadeDelta = 5;
+    fadeSteps = [ 0.03 0.03 ];
+    backend = "glx";
+    vSync = true;
+  };
+}
