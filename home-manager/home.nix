84485deca5a13d407c82e0dd4d9a219c4594accd
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2025-01-19 15:37:12 +0800

    Bugfix syntax error

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -38,7 +38,7 @@
   home = {
     username = "${buildVars.user}";
     homeDirectory = "/home/${buildVars.user}";
-    packages = with pkgs [ brightnessctl ruby python3Full ];
+    packages = with pkgs; [ brightnessctl ruby python3Full ];
     sessionPath = [
       "$HOME/bin"
     ];
