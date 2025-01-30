35805592993fd6e413fc8f6fefd99d8a1bae074f
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2025-01-31 07:57:05 +0800

    Fix XDG syntax error

--- home/common/default.nix
+++ home/common/default.nix
@@ -40,7 +40,7 @@
     enable = true;
     mime.enable = true;
     mimeApps.enable = true;
-  }
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
